bec04de3652f84bbcc9efa220e4eef7f7fb40f65
Stean <stean.ngugi@student.moringaschool.com>
2025-04-03 08:41:45 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let left = parseInt(dodger.style.left) || 0;
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let left = parseInt(dodger.style.left) || 0;
+
+    if (left < 360) { // Assuming the game area width is 400px
+        dodger.style.left = `${left + 10}px`;
+    }
+}
